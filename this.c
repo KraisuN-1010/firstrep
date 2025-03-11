e32cd6d1a1b5f8454e7f1bce59f937708689926d
Uday Gadhavi <gadhaviud@gmail.com>
2025-03-11 19:51:03 +0530

    I added code to find number of digits

--- this.c
+++ this.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 int main(){
-    int num;
+    int num,n=0;
     printf("Enter number");
     scanf("%d",&num);
+    int num2=num;
+    while(num2>0){
+        num2/=10;
+        n++;
+    }
 }
